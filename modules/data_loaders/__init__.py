bd1dc9330837da2465dda0d538d936572703dbcc
RogerM <50708384+ROGMARQ14@users.noreply.github.com>
2025-07-31 14:43:49 -0400

    Update __init__.py

--- modules/data_loaders/__init__.py
+++ modules/data_loaders/__init__.py
@@ -1,10 +1,8 @@
 # modules/data_loaders/__init__.py
 """Data loading modules for various sources"""
 
-from .crawler_loader import CrawlerDataLoader
 from .gsc_loader import GSCLoader
 
 __all__ = [
-    'CrawlerDataLoader',
     'GSCLoader'
 ]
